8d025ee8059b7ffc3e21af655ab8b798c1a63d8c
VELIX <velix2026@gmail.com>
2026-06-21 13:37:01 +0300

    update ChatGPT verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
         <meta name="twitter:creator" content="@velixstore" />
 
         {/* ChatGPT Search Verification */}
-        <meta name="openai-domain-verification" content="dv-2PmqVRRiAdC0ozu60zptZrJ6" />
+        <meta name="openai-domain-verification" content="dv-lSSFBSPcG3WSIkAJHP6Bi45w" />
         <meta name="msvalidate.01" content="" />
         <meta name="google-site-verification" content="" />
 
